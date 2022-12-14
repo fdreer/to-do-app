52e52f9dff310bc11117d423d8b27da39b4ad972
fdreer <francodreer@gmail.com>
2022-12-14 18:55:35 -0300

    Control de errores con sweetAlert

--- scripts/helpers/ajax.js
+++ scripts/helpers/ajax.js
@@ -1,6 +1,6 @@
 export default async function ajax(props) {
 
-    let {method,url,jwt,cbSuccess,body} = props;
+    let {method,url,jwt,body} = props;
 
     const options = {
         method: method,
@@ -11,8 +11,36 @@ export default async function ajax(props) {
         body: JSON.stringify(body)
     }
 
-    const res = await fetch(url, options);
-    const json = await res.json();
+    try{
 
-    cbSuccess(json);
+        const res = await fetch(url, options);
+        const json = await res.json();
+        if (!res.ok) throw {status:res.status}
+        return json
+    } 
+    catch(err){
+        alertError(err);
+    }
+}
+
+function alertError(err) {
+    if (err.status === 400) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Contraseña incorrecta',
+        })
+    } else if(err.status === 404){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'El usuario no existe',
+        })
+    } else if(err.status === 500){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Error del servidor',
+        })
+    }
 }
